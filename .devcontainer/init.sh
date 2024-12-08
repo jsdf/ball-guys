19c3ca1c46af87ad90f26622e1b76a05a551f041
James Friend <james@kitefire.com>
2024-12-08 23:39:33 +0000

    devcontainer

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -euxo pipefail
 
 cd /n64_toolchain || exit
 touch env.sh
@@ -7,12 +7,14 @@ git clone -b preview https://github.com/DragonMinded/libdragon.git &&
     cd libdragon &&
     echo "export LIBDRAGON_VERSION="$(git rev-parse --short HEAD) >>/n64_toolchain/env.sh &&
     ./build.sh
+echo "exit code: $?"
 cd ..
 git clone https://github.com/HailToDodongo/tiny3d.git &&
     cd tiny3d &&
     echo "export TINY3D_VERSION="$(git rev-parse --short HEAD) >>/n64_toolchain/env.sh &&
     export N64_INST=/n64_toolchain &&
     ./build.sh
+echo "exit code: $?"
 cd ..
 echo "export N64_INST=/n64_toolchain" >>env.sh &&
     echo "export T3D_INST=/n64_toolchain/tiny3d" >>env.sh
